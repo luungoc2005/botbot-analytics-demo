8448a04967ef28e09eddcd3e71a4ee639f19880f
luungoc2005 <luungoc2005@gmail.com>
2020-01-18 06:42:58 +0800

    add multiplier to total loss

--- analytics-backend/tasks/modeling/train.py
+++ analytics-backend/tasks/modeling/train.py
@@ -107,12 +107,11 @@ if __name__ == '__main__':
             self.init_weights()
 
         def init_weights(self):
-            initrange = 0.02
-            self.generator_lm.embedding.weight.data.normal_(mean=0.0, std=initrange)
+            initrange = 0.1
+            self.generator_lm.embedding.weight.data.uniform_(-initrange, initrange)
             self.generator_head.decoder.bias.data.zero_()
-
             if not self.tie_decoder:
-                self.generator_head.decoder.weight.data.normal_(mean=0.0, std=initrange)
+                self.generator_head.decoder.weight.data.uniform_(-initrange, initrange)
 
         def forward(self, tokens, input_lengths=None):
             return self.discriminator_lm(tokens, input_lengths)
@@ -176,7 +175,7 @@ if __name__ == '__main__':
                     (x_discriminator > .5).float()[mask_positions] == \
                     adjusted_mask_positions[mask_positions]
                 ).float().mean()
-            loss = generator_loss + discriminator_loss
+            loss = generator_loss + self.discriminator_loss_delta * discriminator_loss
 
             tensorboard_logs = {
                 'train_loss': loss,
@@ -241,7 +240,7 @@ if __name__ == '__main__':
                     adjusted_mask_positions[mask_positions]
                 ).float().mean()
 
-            loss = generator_loss + discriminator_loss
+            loss = generator_loss + self.discriminator_loss_delta * discriminator_loss
 
             result = {
                 'batch_first_item': (x[0], x_lengths[0]),
@@ -330,31 +329,7 @@ if __name__ == '__main__':
             return result
 
         def configure_optimizers(self):
-            num_warmup_steps = 10000
-            num_training_steps = -1
-            weight_decay=0.01
-
-            from torch.optim.lr_scheduler import LambdaLR
-
-            no_decay = ["bias", "LayerNorm.weight"]
-            optimizer_grouped_parameters = [
-                {
-                    "params": [p for n, p in self.named_parameters() if not any(nd in n for nd in no_decay)],
-                    "weight_decay": weight_decay,
-                },
-                {"params": [p for n, p in self.named_parameters() if any(nd in n for nd in no_decay)], "weight_decay": 0.0},
-            ]
-
-            def lr_lambda(current_step):
-                if current_step < num_warmup_steps:
-                    return float(current_step) / float(max(1, num_warmup_steps))
-                return max(
-                    0.0, float(num_training_steps - current_step) / float(max(1, num_training_steps - num_warmup_steps))
-                )
-
-            optimizer = torch.optim.AdamW(optimizer_grouped_parameters, lr=5e-4, eps=1e-6)
-            scheduler = LambdaLR(optimizer, lr_lambda=lr_lambda)
-            return [optimizer], [scheduler]
+            return torch.optim.AdamW(self.parameters(), lr=3e-4)
 
         @pl.data_loader
         def train_dataloader(self):
@@ -384,8 +359,8 @@ if __name__ == '__main__':
 
     MODEL_CONFIG = HParams({
         'generator_lm': {
-            'vocab_size': 12008,
-            'embedding_size': 64,
+            'vocab_size': 12000,
+            'embedding_size': 48,
             'embedding_factor_size': 64,
             'num_attention_heads': 1,
             'max_sequence_length': 128,
@@ -396,11 +371,11 @@ if __name__ == '__main__':
         'generator_head': {
             'encoder_hidden_size': 64,
             'vocab_size': 12008,
-            'embedding_size': 64,
+            'embedding_size': 48,
             'embedding_factor_size': 64
         },
         'discriminator_lm': {
-            'vocab_size': 12008,
+            'vocab_size': 12000,
             'embedding_size': 128,
             'embedding_factor_size': 256,
             'num_attention_heads': 4,
@@ -416,7 +391,7 @@ if __name__ == '__main__':
         },
         'discriminator_loss_delta': 50,
         'tie_encoder': False,
-        'tie_decoder': True
+        'tie_decoder': False
     })
 
     model = LMAdversarialModel(MODEL_CONFIG)
@@ -433,12 +408,11 @@ if __name__ == '__main__':
     )
     trainer = Trainer(
         gpus=1, 
-        use_amp=True,
-        weights_summary='full',
+        use_amp=True, 
         row_log_interval=10,
         early_stop_callback=False,
         checkpoint_callback=checkpoint_callback,
         val_percent_check=0.2,
-        gradient_clip_val=1.0
+        gradient_clip_val=.5,
     )
     trainer.fit(model)
