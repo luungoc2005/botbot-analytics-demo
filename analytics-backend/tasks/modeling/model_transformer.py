3dafe779ee8f8365532a831ee3653e9b856c1c5f
luungoc2005 <luungoc2005@gmail.com>
2020-01-18 03:44:43 +0800

    bug fixes

--- analytics-backend/tasks/modeling/model_transformer.py
+++ analytics-backend/tasks/modeling/model_transformer.py
@@ -9,7 +9,7 @@ import torch.nn.functional as F
 from torch.nn import TransformerEncoder, TransformerEncoderLayer, LayerNorm
 
 DEFAULT_TRANSFORMER_CONFIG = {
-    'vocab_size': 12000,
+    'vocab_size': 12008,
     'embedding_size': 128,
     'embedding_factor_size': 768,
     'num_attention_heads': 4,
@@ -19,6 +19,50 @@ DEFAULT_TRANSFORMER_CONFIG = {
     'dropout': .1
 }
 
+# https://pytorch.org/docs/stable/_modules/torch/nn/modules/transformer.html
+class TiedTransformerEncoder(nn.Module):
+    r"""TransformerEncoder is a stack of N encoder layers
+    "Tied": ALBERT-like sharing of parameters across layers
+
+    Args:
+        encoder_layer: an instance of the TransformerEncoderLayer() class (required).
+        num_layers: the number of sub-encoder-layers in the encoder (required).
+        norm: the layer normalization component (optional).
+
+    Examples::
+        >>> encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=8)
+        >>> transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=6)
+        >>> src = torch.rand(10, 32, 512)
+        >>> out = transformer_encoder(src)
+    """
+
+    def __init__(self, encoder_layer, num_layers, norm=None):
+        super(TiedTransformerEncoder, self).__init__()
+        self.layer = encoder_layer
+        self.num_layers = num_layers
+        self.norm = norm
+
+    def forward(self, src, mask=None, src_key_padding_mask=None):
+            r"""Pass the input through the encoder layers in turn.
+
+            Args:
+                src: the sequnce to the encoder (required).
+                mask: the mask for the src sequence (optional).
+                src_key_padding_mask: the mask for the src keys per batch (optional).
+
+            Shape:
+                see the docs in Transformer class.
+            """
+            output = src
+
+            for i in range(self.num_layers):
+                output = self.layer(output, src_mask=mask,
+                    src_key_padding_mask=src_key_padding_mask)
+
+            if self.norm:
+                output = self.norm(output)
+
+            return output
 
 class TransformerLM(nn.Module):
 
@@ -42,37 +86,44 @@ class TransformerLM(nn.Module):
         )
 
         encoder_layers = TransformerEncoderLayer(
-            self.vocab_size,
+            self.embedding_factor_size,
             self.num_attention_heads, 
             self.dim_feedforward,
             self.dropout
         )
         encoder_norm = LayerNorm(self.embedding_factor_size)
-        self.transformer_encoder = TransformerEncoder(
+        self.transformer_encoder = TiedTransformerEncoder(
             encoder_layers, 
             self.num_layers,
             encoder_norm
         )
-
+        
         self.embedding = nn.Embedding(self.vocab_size, self.embedding_size)
         self.embedding_linear = nn.Linear(self.embedding_size, self.embedding_factor_size)
 
 
     def forward(self, tokens, input_lengths=None):
         x = self.embedding(tokens)
-        x = self.embedding_linear(x)
 
-        x = self.encoder(x) * math.sqrt(self.embedding_factor_size)
+        # since pytorch transformer layers are seq_length first
+        x = x.permute(1, 0, 2)
+
+        x = x * math.sqrt(self.embedding_size)
         x = self.pos_encoder(x)
+
+        x = self.embedding_linear(x)
 
         if input_lengths is None:
             x = self.transformer_encoder(x)
         else:
-            mask = torch.arange(self.max_sequence_length).unsqueeze(0) < input_lengths.unsqueeze(1)
-            mask = torch.where(mask, float('-inf'), 0.)
-            x = self.transformer_encoder(x, mask)
+            mask = torch.arange(self.max_sequence_length).unsqueeze(0).cuda() < input_lengths.unsqueeze(1)
+
+            x = self.transformer_encoder(x, src_key_padding_mask=mask)
         
-        return output
+        # then swap back
+        x = x.permute(1, 0, 2)
+
+        return x
 
 
 class PositionalEncoding(nn.Module):
--- analytics-backend/tasks/modeling/train.py
+++ analytics-backend/tasks/modeling/train.py
@@ -16,7 +16,7 @@ train_dataset_path = path.join(getcwd(), 'tasks/modeling/data/data_train.h5')
 test_dataset_path = path.join(getcwd(), 'tasks/modeling/data/data_test.h5')
 CHECKPOINT_PATH = '/media/luungoc2005/Data/Projects/botbot-analytics-demo/checkpoints'
 VOCAB_PATH = '/home/luungoc2005/Documents/botbot-analytics-demo/analytics-backend/tasks/modeling/data/sentencepiece/en-vocab.txt'
-BATCH_SIZE = 80
+BATCH_SIZE = 128
 NUM_WORKERS = 7
 tokenizer = None
 
@@ -110,6 +110,7 @@ if __name__ == '__main__':
             initrange = 0.1
             self.generator_lm.embedding.weight.data.uniform_(-initrange, initrange)
             self.generator_head.decoder.bias.data.zero_()
+
             if not self.tie_decoder:
                 self.generator_head.decoder.weight.data.uniform_(-initrange, initrange)
 
@@ -329,7 +330,30 @@ if __name__ == '__main__':
             return result
 
         def configure_optimizers(self):
-            return torch.optim.AdamW(self.parameters(), lr=3e-4)
+            num_warmup_steps = 10000
+            weight_decay=0.01
+
+            from torch.optim.lr_scheduler import LambdaLR
+
+            no_decay = ["bias", "LayerNorm.weight"]
+            optimizer_grouped_parameters = [
+                {
+                    "params": [p for n, p in self.named_parameters() if not any(nd in n for nd in no_decay)],
+                    "weight_decay": weight_decay,
+                },
+                {"params": [p for n, p in self.named_parameters() if any(nd in n for nd in no_decay)], "weight_decay": 0.0},
+            ]
+
+            def lr_lambda(current_step):
+                if current_step < num_warmup_steps:
+                    return float(current_step) / float(max(1, num_warmup_steps))
+                return max(
+                    0.0, float(num_training_steps - current_step) / float(max(1, num_training_steps - num_warmup_steps))
+                )
+
+            optimizer = torch.optim.AdamW(optimizer_grouped_parameters, lr=1e-4, eps=1e-6)
+            scheduler = LambdaLR(optimizer, lr_lambda=lr_lambda)
+            return [optimizer], [scheduler]
 
         @pl.data_loader
         def train_dataloader(self):
@@ -359,8 +383,8 @@ if __name__ == '__main__':
 
     MODEL_CONFIG = HParams({
         'generator_lm': {
-            'vocab_size': 12000,
-            'embedding_size': 48,
+            'vocab_size': 12008,
+            'embedding_size': 64,
             'embedding_factor_size': 64,
             'num_attention_heads': 1,
             'max_sequence_length': 128,
@@ -371,11 +395,11 @@ if __name__ == '__main__':
         'generator_head': {
             'encoder_hidden_size': 64,
             'vocab_size': 12008,
-            'embedding_size': 48,
+            'embedding_size': 64,
             'embedding_factor_size': 64
         },
         'discriminator_lm': {
-            'vocab_size': 12000,
+            'vocab_size': 12008,
             'embedding_size': 128,
             'embedding_factor_size': 256,
             'num_attention_heads': 4,
@@ -391,7 +415,7 @@ if __name__ == '__main__':
         },
         'discriminator_loss_delta': 50,
         'tie_encoder': False,
-        'tie_decoder': False
+        'tie_decoder': True
     })
 
     model = LMAdversarialModel(MODEL_CONFIG)
@@ -408,11 +432,12 @@ if __name__ == '__main__':
     )
     trainer = Trainer(
         gpus=1, 
-        use_amp=True, 
+        use_amp=True,
+        weights_summary='full',
         row_log_interval=10,
         early_stop_callback=False,
         checkpoint_callback=checkpoint_callback,
         val_percent_check=0.2,
-        gradient_clip_val=.5,
+        gradient_clip_val=1.0
     )
     trainer.fit(model)
